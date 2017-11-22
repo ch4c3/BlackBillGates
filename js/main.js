865f846d5b9365beba250733ce632cddf4f4b48c
MosesKing <mkc42@mail.missouri.edu>
2017-11-21 21:49:04 -0600

    Updates
    Secured the API Key from the owner of OMDb. Updated Code and the Search Function for FIlms/Movies now works. Next is getting it to display. See Console for results form.

--- js/main.js
+++ js/main.js
@@ -7,8 +7,29 @@ $(document).ready(() => {
 });
 function getMovies(searchText){
   // waiting for guy to send me api key
-  axios.get('http://www.omdbapi.com?s='+searchText)
-  .then((response) => {})
-  .catch(err) => {
-    console.log(err);};
+  axios.get('http://www.omdbapi.com/?s='+searchText+'&apikey=ce021223')
+  .then((response) => {
+      // Success
+      console.log(searchText);
+      console.log(response);
+  })
+  .catch((error) => {
+      // Error
+      if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          // console.log(error.response.data);
+          // console.log(error.response.status);
+          // console.log(error.response.headers);
+      } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+      } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', error.message);
+      }
+      console.log(error.config);
+  });
 }
