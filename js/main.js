7cad79bcb06fa1dac1b941681e142066cf1a9322
MosesKing <mkc42@mail.missouri.edu>
2017-11-24 23:46:12 -0600

    Updates to Project
    Here's What's Changed:
    
    -Added Displays of the Posters
    -Optimized (not perfect) for Mobile
    - Added Buttons and Same Heights for each of the columns (regardless of title height and length (not perfect)
    - Added function to send data to SessionStorage to be able to look into information about individual films
    -Added front-end layout to the second page; which gives the user additional information about the selected film.
    - Added a way to go back to home page
    - Added additional button within the description of each page to visit the official IMDb page which opens a blank tab to the same film using the film IMDb ID.
    - Added Error Handling to the necessary functions.

--- js/main.js
+++ js/main.js
@@ -6,28 +6,91 @@ $(document).ready(() => {
   });
 });
 function getMovies(searchText){
-  // waiting for guy to send me api key
+
   axios.get('http://www.omdbapi.com/?s='+searchText+'&apikey=ce021223')
   .then((response) => {
-      // Success
-      console.log(searchText);
+      // Success Case
       console.log(response);
+      let movies = response.data.Search;
+      let output = '';
+      $.each(movies, (index, movie) => {
+        output += `<div class="col-md-3">
+        <div class="well text-center">
+        <img src="${movie.Poster}">
+        <h5>${movie.Title}</h5>
+        <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+        </div>
+        </div>
+        `;
+      });
+      // Throws the output onto the page
+      $('#movies').html(output);
+  })
+  .catch((error) => {
+      // Error Case
+      if (error.response) {
+      } else if (error.request) {
+          console.log(error.request);
+      } else {
+          console.log('Error', error.message);
+      }
+      console.log(error.config);
+  });
+}
+// when the user has selected a movie this funciton will be called
+function movieSelected(id){
+  sessionStorage.setItem('movieID', id);
+  window.location = 'movie.html';
+  return false;
+}
+function getMovie(){
+  let movieID = sessionStorage.getItem('movieID');
+
+ // change the axios get html from s to i for the individual film
+  axios.get('http://www.omdbapi.com/?i='+movieID+'&apikey=ce021223')
+  .then((response) => {
+      // Success
+      console.log(response);
+      let movie = response.data;
+
+      let output =`
+      <div class="row">
+        <div class="col-md-4">
+          <img src="${movie.Poster}" class="thumbnail">
+        </div>
+        <div class="col-md-8">
+          <h2>${movie.Title}</h2>
+          <ul class="list-group">
+          <li class="list-group-item>"<strong>Genre:</strong>${movie.Genre}<li>
+          <li class="list-group-item>"<strong>Released:</strong>${movie.Released}<li>
+          <li class="list-group-item>"<strong>Rated:</strong>${movie.Rated}<li>
+          <li class="list-group-item>"<strong>IMDb Rating:</strong>${movie.imdbRating}<li>
+          <li class="list-group-item>"<strong>Director:</strong>${movie.Director}<li>
+          <li class="list-group-item>"<strong>Writer:</strong>${movie.Writer}<li>
+          <li class="list-group-item>"<strong>Actors:</strong>${movie.Actors}<li>
+          </ul>
+        </div>
+        </div>
+        <div class="row">
+        <div class="well">
+            <br>
+          <h3>Plot</h3>
+          ${movie.Plot}
+            <hr>
+            <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDb</a>
+            <a href="index.html" class="btn btn-primary">Go Back To Search</a>
+        </div>
+        </div>
+      `;
+      // throws the output onto the page
+        $('#movie').html(output);
   })
   .catch((error) => {
       // Error
       if (error.response) {
-          // The request was made and the server responded with a status code
-          // that falls out of the range of 2xx
-          // console.log(error.response.data);
-          // console.log(error.response.status);
-          // console.log(error.response.headers);
       } else if (error.request) {
-          // The request was made but no response was received
-          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-          // http.ClientRequest in node.js
           console.log(error.request);
       } else {
-          // Something happened in setting up the request that triggered an Error
           console.log('Error', error.message);
       }
       console.log(error.config);
